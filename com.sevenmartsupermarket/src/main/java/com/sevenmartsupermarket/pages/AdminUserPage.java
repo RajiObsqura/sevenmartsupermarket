15f13cdc82299a997417482d16e3337df5621bed
RajiObsqura <rajirn@gmail.com>
2024-05-21 15:09:11 +0530

    committed admin

--- com.sevenmartsupermarket/src/main/java/com/sevenmartsupermarket/pages/AdminUserPage.java
+++ com.sevenmartsupermarket/src/main/java/com/sevenmartsupermarket/pages/AdminUserPage.java
@@ -1,7 +1,11 @@
 package com.sevenmartsupermarket.pages;
 
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -9,16 +13,20 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
 import com.sevenmartsupermarket.constants.Constants;
+import com.sevenmartsupermarket.utilities.GeneralUtilities;
 import com.sevenmartsupermarket.utilities.PageUtilities;
 
 public class AdminUserPage {
 	WebDriver driver;
 	PageUtilities pageutility;
-
+	GeneralUtilities  generalutilitiy = new GeneralUtilities();
+	
 	@FindBy(xpath = "(//a[@class='small-box-footer'])[2]")
 	WebElement adminbutton;
 	@FindBy(xpath = "(//i[@class='fas fa-edit'])[2]")
 	WebElement newbutton ;
+	@FindBy(xpath="//a[@class='btn btn-rounded btn-danger']")
+	WebElement newUserAdd;
 	//webelements for inputting data
 	@FindBy(xpath="//input[@id='un']")
 	WebElement username;
@@ -37,6 +45,29 @@ public class AdminUserPage {
 	WebElement usertype;
 	@FindBy(xpath="(//button[@type='submit'])[1]")
 	WebElement searchuserbutton;
+	@FindBy(xpath="//table[@class='table table-bordered table-hover table-sm']//tbody//tr[1]//td[1]")
+	WebElement getuserData;
+	@FindBy(xpath="//table[@class='table table-bordered table-hover table-sm']/tbody//tr[1]//td")
+	WebElement nameElements;
+	
+	@FindBy(xpath="//input[@id='username']")
+	WebElement userNameField;
+	@FindBy(xpath="//input[@id='password']")
+	WebElement passwordField;
+	@FindBy(xpath = "//select[@id='user_type']")
+	 WebElement userTypeDropdown;
+	@FindBy(xpath = "(//button[@class='btn btn-block-sm btn-danger'])[2]")
+	private WebElement saveButtonElement;
+	@FindBy(xpath = "//div[@class='alert alert-success alert-dismissible']")
+	private WebElement alertMessageUserCreation;
+	
+	//deactivate
+	@FindBy(xpath = "//table[@class='table table-bordered table-hover table-sm']//tr//td[1]")
+	private List<WebElement> usertablenamelist;
+	
+	//List users
+	@FindBy(xpath = "//tbody//tr//td[1]")
+	List<WebElement> listAllUsers;
 	
 	public AdminUserPage(WebDriver driver) {
 		this.driver = driver;
@@ -48,7 +79,7 @@ public class AdminUserPage {
 	}
 
 	public void newButtonClick() {
-		newbutton.click();
+		newUserAdd.click();
 	}
 	public void  getUserNameClick()
 	{
@@ -85,6 +116,66 @@ public class AdminUserPage {
 	public void searchClick()
 	{
 		 searchuserbutton.click();
+	}
+	public String verifyUserdata()
+	{
+		return getuserData.getText();
+	}
+	public void deActivatemethod()
+	{
+		
+	}
+	public void listAllUsers()
+	{
+		for (WebElement e1:listAllUsers)
+			System.out.println(e1.getText());
+			
+		
+	}
+	
+	public String getTextAlertMessage() {
+		String a=alertMessageUserCreation.getText();
+		return a;
+	}
+	public void saveUser(String username,String password,String usertype)
+	{
+		enterUserName(username);
+		enterPassword(password);
+		selectUserType(usertype);
+		saveButtonElement.click();
+	}
+	public void deactivateUser(String name) {
+		pageutility=new PageUtilities(driver);
+		List<String> namelist=new ArrayList<String>();
+		int a=usertablenamelist.size();
+		namelist=generalutilitiy.gettextOfelements(usertablenamelist);
+		//System.out.println(namelist);
+		int index=0;
+		for(index=0;index<namelist.size();index++)
+		{
+			if(namelist.get(index).equals(name)) {
+				index++;
+				break;
+			}
+		}
+		WebElement deactivateButton=driver.findElement(By.xpath("//table//tr["+index+"]//td[5]//a[1]"));	
+		pageutility.scrollAndClick(deactivateButton);
+
+		
+		
+	}
+	public void enterUserName(String username )
+	{
+		userNameField.sendKeys(username);
+	}
+	public void enterPassword(String password )
+	{
+		passwordField.sendKeys(password);
+	}
+	public void selectUserType(String text)
+	{
+		//pageutility.select_ByVisibleText(userTypeDropdown, text);
+		userTypeDropdown.sendKeys(text);
 	}
 	}
 
