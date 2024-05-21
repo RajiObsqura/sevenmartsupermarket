e3dd8bc16b42d8843e200d06ecb55723a88ab976
RajiObsqura <rajirn@gmail.com>
2024-05-21 15:11:33 +0530

    commited home

--- com.sevenmartsupermarket/src/main/java/com/sevenmartsupermarket/pages/HomePage.java
+++ com.sevenmartsupermarket/src/main/java/com/sevenmartsupermarket/pages/HomePage.java
@@ -2,6 +2,8 @@ package com.sevenmartsupermarket.pages;
 
 import java.io.FileInputStream;
 import java.util.Properties;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -9,25 +11,33 @@ import org.openqa.selenium.support.PageFactory;
 
 import com.sevenmartsupermarket.constants.Constants;
 
+public class HomePage {
+	WebDriver driver;
 
- public class HomePage {
-	 WebDriver driver;
-		
-		@FindBy(xpath ="//a[@class='d-block']" )
-		WebElement userProfileElement;
-		@FindBy(xpath="//p[text()='Push Notifications']")
-		private WebElement pushbutton;
-		
-		public HomePage(WebDriver driver) {
-			this.driver = driver;
-			PageFactory.initElements(driver, this);
-		}
-		
-		public String getProfileName() {
-			return userProfileElement.getText();
-		}
-		public void pushNotificationClick()
-		{
-			 pushbutton.click();		}
+	@FindBy(xpath = "//a[@class='d-block']")
+	WebElement userProfileElement;
+	@FindBy(xpath = "//p[text()='Push Notifications']")
+	private WebElement pushbutton;
+	@FindBy(xpath = "//ul//li[3]")
+	private WebElement manageProductElement;
+	By homePageWaitElement=By.xpath("//a[@class='d-block']");
+    
+	public HomePage(WebDriver driver) 
+	{
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
-	
+
+	public String getProfileName() {
+		return userProfileElement.getText();
+	}
+
+	public void pushNotificationClick() {
+		pushbutton.click();
+	}
+
+	public void clickOnManageProduct() {
+		manageProductElement.click();
+	}
+
+}
