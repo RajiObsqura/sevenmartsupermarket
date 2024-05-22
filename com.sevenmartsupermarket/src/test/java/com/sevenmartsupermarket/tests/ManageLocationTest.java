df730e638a3d81b1f0bc60c9251901369c766653
RajiObsqura <rajirn@gmail.com>
2024-05-22 14:20:54 +0530

    manage module

---
+++ com.sevenmartsupermarket/src/test/java/com/sevenmartsupermarket/tests/ManageLocationTest.java
@@ -0,0 +1,46 @@
+package com.sevenmartsupermarket.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.sevenmartsupermarket.base.Base;
+import com.sevenmartsupermarket.pages.HomePage;
+import com.sevenmartsupermarket.pages.LoginPage;
+import com.sevenmartsupermarket.pages.ManageLocationPage;
+
+
+
+public class ManageLocationTest extends Base
+{
+	LoginPage loginpage;
+	HomePage homepage;
+	ManageLocationPage managelocationpage;
+	
+	@Test 
+		public void managelocationClick()
+	{
+		loginpage = new LoginPage(driver);
+		loginpage.login();
+		homepage = new HomePage(driver);
+		managelocationpage=new ManageLocationPage(driver);
+		managelocationpage.manageLocationButtonclick();
+	
+	}
+	
+	@Test
+	public void addLocation()
+	{
+		loginpage = new LoginPage(driver);
+		loginpage.login();
+		homepage = new HomePage(driver);
+		managelocationpage=new ManageLocationPage(driver);
+		managelocationpage.manageLocationButtonclick();
+		managelocationpage.newButtonClick();
+		managelocationpage.addNewLocation();
+		System.out.println("TeXT");
+		//managelocationpage.alertCheckForAddLocation();
+		String actualAlertText=managelocationpage.alertCheckForAddLocation();
+		String expectedAlertText="Location Created Successfully";
+		Assert.assertEquals(actualAlertText, expectedAlertText);
+	}
+}
---
+++ com.sevenmartsupermarket/src/test/java/com/sevenmartsupermarket/tests/ManageProductTest.java
@@ -0,0 +1,60 @@
+package com.sevenmartsupermarket.tests;
+
+import org.testng.annotations.Test;
+
+import com.sevenmartsupermarket.pages.HomePage;
+import com.sevenmartsupermarket.pages.LoginPage;
+import com.sevenmartsupermarket.pages.ManageProductPage;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import com.sevenmartsupermarket.base.Base;
+import com.sevenmartsupermarket.pages.AdminUserPage;
+import com.sevenmartsupermarket.utilities.ExcelReader;
+import com.sevenmartsupermarket.utilities.GeneralUtilities;
+
+
+public class ManageProductTest extends Base {
+	LoginPage loginpage;
+	HomePage homepage;
+	ManageProductPage manageproductpage;
+	//private WebDriver driver;
+	
+	@Test
+	public void manageproductclick()
+	{  
+		loginpage = new LoginPage(driver);
+		loginpage.login();
+		homepage = new HomePage(driver);
+		manageproductpage=new ManageProductPage(driver);
+		manageproductpage.manageProductButtonclick();
+	}
+    
+	@Test
+	public void searchItem()
+	{
+		loginpage = new LoginPage(driver);
+		loginpage.login();
+		homepage = new HomePage(driver);
+		manageproductpage=new ManageProductPage(driver);
+		manageproductpage.manageProductButtonclick();
+		manageproductpage.searchForProductClick();
+		manageproductpage.searchElementTitle();
+		manageproductpage.searchForProductitem();
+		String actualItemName=manageproductpage.verifyTableItemdata();
+		String expectedItemName="Biriyani";
+		Assert.assertEquals(actualItemName, expectedItemName);
+	}
+	@Test
+public void deleteItem()
+{
+	loginpage = new LoginPage(driver);
+	loginpage.login();
+	homepage = new HomePage(driver);
+	manageproductpage=new ManageProductPage(driver);
+	manageproductpage.manageProductButtonclick();
+}
+
+
+
+}
