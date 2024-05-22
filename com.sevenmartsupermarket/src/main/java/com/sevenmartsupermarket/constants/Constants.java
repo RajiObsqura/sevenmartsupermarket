fa19c17ca26f5112fd5a2f364252a0829c530fbe
RajiObsqura <rajirn@gmail.com>
2024-05-22 14:25:41 +0530

    constants

--- com.sevenmartsupermarket/src/main/java/com/sevenmartsupermarket/constants/Constants.java
+++ com.sevenmartsupermarket/src/main/java/com/sevenmartsupermarket/constants/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
 	public static final long IMPLICIT_WAIT=10;
 	public static final String CONFIG_FILE_PATH = System.getProperty("user.dir")+"\\src\\main\\resources\\config.properties";
 	public static final String Excel_FILE_PATH = System.getProperty("user.dir")+"\\src\\main\\resources\\ExcelFiles\\";
-	
+	public static final String ScreenShot_FILE_PATH=System.getProperty("user.dir")+"\\Screenshots\\";
+	public static final String EXTENT_REPORT_FILE_PATH=System.getProperty("user.dir")+"\\ExtentReport";
 }
