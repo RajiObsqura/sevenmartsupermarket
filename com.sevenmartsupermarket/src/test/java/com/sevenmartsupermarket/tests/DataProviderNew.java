1b4efd3a3942c6c8c9e3303ac1caa35d897882cf
RajiObsqura <rajirn@gmail.com>
2024-05-22 14:19:42 +0530

    dataprovider

--- com.sevenmartsupermarket/src/test/java/com/sevenmartsupermarket/tests/DataProviderNew.java
+++ com.sevenmartsupermarket/src/test/java/com/sevenmartsupermarket/tests/DataProviderNew.java
@@ -2,17 +2,29 @@ package com.sevenmartsupermarket.tests;
 
 import org.testng.annotations.DataProvider;
 
-public class DataProviderNew {
+import com.sevenmartsupermarket.utilities.ExcelReader;
 
-	@DataProvider(name="usertypelist")
-	public Object[][] usertypelist()
-	{
-		return new Object [][] {{"staff"}};
-	}
-	@DataProvider(name="SearchData")
+  public class DataProviderNew {
+ ExcelReader excelread=new ExcelReader();
+	
+ 
+
+   @DataProvider(name="SearchData")
 	public Object[][] searchdetails()
 	{
-		return new Object [][] {{"raj","db"},
-			                    {"tomy","admin"}};
+	return new Object [][] {{"raj","db"},{"tomy","admin"}};
+		
 	}
+   
+   //this dataprovider is used to read data from the excel .so need to create a ExcelReader object and that object calls the excel sheet .we nned to give the matrix size in our sheet.
+   @DataProvider(name="NewSearchData")
+	public Object[][] searchdetailsNew()
+	{
+	
+		excelread.setExcelFile("UserCreationData","SearchDataWithExcel");
+		return excelread.getMultidimentionalData(2,2);
+	}
+	
 }
+ 
+  
